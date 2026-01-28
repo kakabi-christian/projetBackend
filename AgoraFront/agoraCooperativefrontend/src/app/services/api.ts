3de9445e2d2c6ef335880fb78f5c9b018024faa3
Kakabi Christian <kakabichristian@gmail.com>
2026-01-28 11:08:23 +0100

    ajout de mon  url du backend railWay

--- AgoraFront/agoraCooperativefrontend/src/app/services/api.ts
+++ AgoraFront/agoraCooperativefrontend/src/app/services/api.ts
@@ -1,7 +1,15 @@
-//src/services/api.ts
+// src/services/api.ts
+
+// On vérifie si l'application tourne sur localhost ou sur internet
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8000/api',
-  // Tu peux aussi ajouter l'URL de base pour les images/logos ici
-  storageUrl: 'http://localhost:8000/storage'
+  // Si c'est localhost, on utilise le port 8000, sinon on utilise Railway
+  baseUrl: isLocalhost 
+    ? 'http://localhost:8000/api' 
+    : 'https://sweet-joy-production.up.railway.app/api',
+
+  storageUrl: isLocalhost 
+    ? 'http://localhost:8000/storage' 
+    : 'https://sweet-joy-production.up.railway.app/storage',
 };
-// src/app/services/api.ts
